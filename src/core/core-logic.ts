ba2e6f3a5921d3aefe1aa2525da90f7743393e7c
xicshen <xicshen@expediagroup.com>
2025-08-05 22:31:31 -0400

    5y 40 91 2

--- src/core/core-logic.ts
+++ src/core/core-logic.ts
@@ -63,8 +63,8 @@ export const rebalance = (before: PortfolioSnapshot, simulation: Simulation, mar
     after.nextTarget = before.investments.total * (1 + 0.2);
   } else if (isSpike) {
     rebalanceType = RebalanceType.Spike;
-    after.investments.TQQQ = before.investments.total * targetRatio * 0.75;
-    after.investments.cash = before.investments.total * (1 - targetRatio * 0.75);
+    after.investments.TQQQ = before.investments.total * targetRatio * 1;
+    after.investments.cash = before.investments.total * (1 - targetRatio * 1);
     after.investments.total = before.investments.total;
     after.investments.ratio = after.investments.TQQQ / after.investments.total;
     after.nextTarget = before.investments.total * (1 + 0.2);
